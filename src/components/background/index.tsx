fb54896c3e2d31b4e5c6bca59bd5106206212e2c
Alexander Soldatov <inc1uder@ya.ru>
2022-05-02 11:26:39 +0300

    feat: new background

--- src/components/background/index.tsx
+++ src/components/background/index.tsx
@@ -1,14 +1,16 @@
-import { memo, useMemo, useRef } from "react";
+import { memo, useCallback, useEffect, useMemo, useRef } from "react";
 import clsx from "clsx";
 import { useDidMount } from "rooks";
 import {
+  Color,
+  DoubleSide,
   Mesh,
   MeshBasicMaterial,
   PerspectiveCamera,
   Scene,
-  SphereGeometry,
   WebGLRenderer,
 } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 import styles from "./index.module.scss";
 
@@ -19,41 +21,75 @@ type Props = {
 export const Background = memo<Props>(({ className }) => {
   const ref = useRef<HTMLDivElement>(null);
   const scene = useMemo(() => new Scene(), []);
-  const camera = useMemo(() => new PerspectiveCamera(75, 1, 0.1, 1000), []);
-  const geometry = new SphereGeometry();
-  const material = new MeshBasicMaterial({ color: 0xffffff, wireframe: true });
-  const cube = new Mesh(geometry, material);
-
-  const renderer = new WebGLRenderer({
-    alpha: true,
+  const camera = useMemo(() => new PerspectiveCamera(75, 1, 0.1, 2000), []);
+  const loader = new GLTFLoader().setPath("/models/");
+  const material = new MeshBasicMaterial({
+    side: DoubleSide,
+    transparent: true,
+    opacity: 0.6,
+    color: new Color(0xb1139b),
   });
+
+  const renderer = useMemo(
+    () =>
+      new WebGLRenderer({
+        alpha: true,
+      }),
+    []
+  );
+
+  function animate(mesh: Mesh) {
+    requestAnimationFrame(animate.bind(null, mesh));
+    mesh.rotation.y += 0.0025;
+
+    renderer.render(scene, camera);
+  }
 
   function init() {
     if (ref.current === null) {
       return;
     }
 
-    scene.add(cube);
+    loader.load("bb2.gltf", (gltf) => {
+      gltf.scene.scale.set(1.5, 1.5, 1.5);
+      scene.add(gltf.scene);
+
+      const mesh = gltf.scene.children[0] as Mesh;
+      mesh.material = material;
+
+      animate(mesh);
+    });
+
+    // scene.add(cube);
     camera.aspect = ref.current.clientWidth / ref.current.clientHeight;
     camera.position.z = 5;
     camera.updateProjectionMatrix();
 
     renderer.setSize(ref.current.clientWidth, ref.current.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
     ref.current.appendChild(renderer.domElement);
-
-    function animate() {
-      requestAnimationFrame(animate);
-
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
-      renderer.render(scene, camera);
-    }
-
-    animate();
+    renderer.render(scene, camera);
   }
 
+  const onWindowResize = useCallback(() => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer.render(scene, camera);
+  }, [camera, renderer, scene]);
+
   useDidMount(init);
+
+  useEffect(() => {
+    window.addEventListener("resize", onWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", onWindowResize);
+    };
+  }, [onWindowResize]);
 
   return <div ref={ref} className={clsx(className, styles.container)} />;
 });

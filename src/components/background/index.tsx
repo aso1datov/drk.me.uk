d554e5dccfde59decabccc45a4d3417f4f5af615
Alexander Soldatov <inc1uder@ya.ru>
2022-05-04 22:02:37 +0300

    refactor: improve background

--- src/components/background/index.tsx
+++ src/components/background/index.tsx
@@ -1,8 +1,6 @@
-import { memo, useCallback, useEffect, useMemo, useRef } from "react";
+import { memo, useCallback, useEffect, useRef } from "react";
 import clsx from "clsx";
-import { useDidMount } from "rooks";
 import {
-  Color,
   DoubleSide,
   Mesh,
   MeshBasicMaterial,
@@ -10,7 +8,10 @@ import {
   Scene,
   WebGLRenderer,
 } from "three";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+import { LOGO_COLOR } from "../../constants";
 
 import styles from "./index.module.scss";
 
@@ -18,78 +19,90 @@ type Props = {
   className?: string;
 };
 
+const scene = new Scene();
+const camera = new PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  2000
+);
+
+const material = new MeshBasicMaterial({
+  side: DoubleSide,
+  transparent: true,
+  opacity: 0.65,
+  color: LOGO_COLOR,
+});
+
+const renderer = new WebGLRenderer({
+  alpha: true,
+});
+
+const dracoLoader = new DRACOLoader();
+dracoLoader
+  .setDecoderConfig({ type: "js" })
+  .setDecoderPath("https://www.gstatic.com/draco/v1/decoders/");
+
+const loader = new GLTFLoader();
+loader.setPath("/models/").setDRACOLoader(dracoLoader);
+
+function animate(mesh: Mesh) {
+  requestAnimationFrame(animate.bind(null, mesh));
+  mesh.rotation.y += 0.0025;
+
+  renderer.render(scene, camera);
+}
+
 export const Background = memo<Props>(({ className }) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const scene = useMemo(() => new Scene(), []);
-  const camera = useMemo(() => new PerspectiveCamera(75, 1, 0.1, 2000), []);
-  const loader = new GLTFLoader().setPath("/models/");
-  const material = new MeshBasicMaterial({
-    side: DoubleSide,
-    transparent: true,
-    opacity: 0.6,
-    color: new Color(0xb1139b),
-  });
+  const containerRef = useRef<HTMLDivElement>(null);
 
-  const renderer = useMemo(
-    () =>
-      new WebGLRenderer({
-        alpha: true,
-      }),
-    []
-  );
-
-  function animate(mesh: Mesh) {
-    requestAnimationFrame(animate.bind(null, mesh));
-    mesh.rotation.y += 0.0025;
-
-    renderer.render(scene, camera);
-  }
-
-  function init() {
-    if (ref.current === null) {
+  const setup = useCallback((init?: boolean) => {
+    if (containerRef.current === null) {
       return;
     }
 
-    loader.load("logo.gltf", (gltf) => {
-      gltf.scene.scale.set(1.5, 1.5, 1.5);
+    const container = containerRef.current;
+
+    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.position.z = 5;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    renderer.render(scene, camera);
+
+    if (init) {
+      container.appendChild(renderer.domElement);
+    }
+  }, []);
+
+  const render = useCallback(() => {
+    setup(true);
+
+    loader.load("logo.glb", (gltf) => {
       scene.add(gltf.scene);
+      scene.scale.set(1.5, 1.5, 1.5);
 
       const mesh = gltf.scene.children[0] as Mesh;
       mesh.material = material;
 
       animate(mesh);
     });
+  }, [setup]);
 
-    // scene.add(cube);
-    camera.aspect = ref.current.clientWidth / ref.current.clientHeight;
-    camera.position.z = 5;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(ref.current.clientWidth, ref.current.clientHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
-
-    ref.current.appendChild(renderer.domElement);
-    renderer.render(scene, camera);
-  }
-
-  const onWindowResize = useCallback(() => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    renderer.render(scene, camera);
-  }, [camera, renderer, scene]);
-
-  useDidMount(init);
+  const onWindowResize = useCallback(() => setup(), [setup]);
 
   useEffect(() => {
     window.addEventListener("resize", onWindowResize);
+    render();
 
     return () => {
       window.removeEventListener("resize", onWindowResize);
     };
-  }, [onWindowResize]);
+  }, [onWindowResize, render]);
 
-  return <div ref={ref} className={clsx(className, styles.container)} />;
+  return (
+    <div ref={containerRef} className={clsx(className, styles.container)} />
+  );
 });
--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,11 @@
+import { Color } from "three";
+
 export const ROOT_ELEMENT_ID = "app";
 
 export const ROUTES = {
   home: "/",
   contacts: "/contacts",
 };
+
+export const LOGO_COLOR = new Color(0xf72585);
+// export const LOGO_COLOR = new Color(0xf72585);
